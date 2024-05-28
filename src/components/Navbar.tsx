1763fbf64cb601b0bb97b2b8cef7fd9ebea1d460
Luka Tskhvaradze <130844113+Cxvarisorka@users.noreply.github.com>
2024-05-29 03:42:57 +0400

    Responsive

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 // src/Navbar.tsx
-import React from "react";
+import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
 
@@ -20,6 +20,8 @@ const NavItem: React.FC<NavItemProps> = ({ text, path, t }) => {
 };
 
 const Navbar: React.FC = () => {
+    const [isOpen, setIsOpen] = useState<boolean>(false);
+
     const navItems = [
         { text: 'home', path: '/' },
         { text: 'aboutus', path: '/aboutus' },
@@ -36,24 +38,28 @@ const Navbar: React.FC = () => {
 
     return (
         <header className="bg-green-600 text-white shadow-md">
-        <nav className="2xl:container relative mx-auto py-4 px-3 text-sm lg:text-base flex justify-between items-center">
-            <h1 className="font-bold text-xl lg:text-4xl">
-            <Link to={'/'}>Goa</Link>
-            </h1>
-            <ul className="flex gap-4">
-            {navItems.map(navObj => <NavItem t={t} key={navObj.text} {...navObj} />)}
-            </ul>
-            <div className="flex gap-2">
-            <button 
-                onClick={() => changeLanguage('en')}>
-                English
-            </button>
-            <button 
-                onClick={() => changeLanguage('ka')}>
-                Georgian
-            </button>
-            </div>
-        </nav>
+            <nav className="2xl:container relative mx-auto py-4 px-3 text-sm lg:text-base flex justify-between items-center">
+                <h1 className="font-bold text-xl lg:text-4xl">
+                    <Link to={'/'}>Goa</Link>
+                </h1>
+
+                <button className="md:hidden block" onClick={() => setIsOpen(curValue => !curValue)}>Menu</button>
+
+                <ul className={`flex md:flex-row flex-col md:p-0 p-5 gap-4 md:static absolute md:bg-transparent ${isOpen ? 'left-0 opacity-100' : '-left-full opacity-0' } bg-green-800 md:top-0 top-full md:w-auto w-full duration-300`}>
+                    {navItems.map(navObj => <NavItem t={t} key={navObj.text} {...navObj} />)}
+                </ul>
+
+                <div className="flex gap-2">
+                    <button 
+                        onClick={() => changeLanguage('en')}>
+                        English
+                    </button>
+                    <button 
+                        onClick={() => changeLanguage('ka')}>
+                        Georgian
+                    </button>
+                </div>
+            </nav>
         </header>
     );
 };
